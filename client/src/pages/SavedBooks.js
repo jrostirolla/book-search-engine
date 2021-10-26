c06e3d6143d3b3fa83915c80f3e513b2b3b8739f
James Rostirolla <j_rostirolla@hotmail.com>
2021-10-26 22:49:31 +1100

    fixing github files

---
+++ client/src/pages/SavedBooks.js
@@ -0,0 +1,125 @@
+import React, { useState, useEffect } from 'react';
+import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+
+import { useMutation, useQuery } from '@apollo/client';
+import { GET_ME } from '../utils/queries';
+import { REMOVE_BOOK } from '../utils/mutations';
+
+import { getMe, deleteBook } from '../utils/API';
+import Auth from '../utils/auth';
+import { removeBookId } from '../utils/localStorage';
+
+const SavedBooks = () => {
+  const [userData, setUserData] = useState({});
+
+  // use this to determine if `useEffect()` hook needs to run again
+  const userDataLength = Object.keys(userData).length;
+
+  const { loading, data, error } = useQuery(GET_ME, {
+    variables: { username: userData.username }
+  });
+
+// console.log(userData)
+
+  if (loading) {
+    console.log('data loading...')
+  };
+
+  if (error) {
+    console.log(error)
+  };
+
+  useEffect((userData) => {
+    const GetUserData = async () => {
+
+        const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+        if (!token) {
+          return false;
+        }
+
+        const response = await getMe(token);
+
+        // if (!response.ok) {
+        //   throw new Error('something went wrong!');
+        // }
+
+        const user = await response.json();
+        setUserData(user);
+      }
+
+    GetUserData();
+  }, [userDataLength]);
+
+  // create function that accepts the book's mongo _id value as param and deletes the book from the database
+  
+  
+
+  const HandleDeleteBook = async (bookId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+      const { loading, data, error } = useMutation(REMOVE_BOOK, {
+        variables: {bookId: bookId}
+      });
+      
+    if (!token) {
+      return false;
+    }
+
+    // try {
+
+
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
+
+      const updatedUser = data;
+      setUserData(updatedUser);
+      // upon success, remove book's id from localStorage
+      removeBookId(bookId);
+    // } catch (err) {
+    //   console.error(err);
+    // }
+  };
+
+  // if data isn't here yet, say so
+  if (!userDataLength) {
+    return <h2>LOADING...</h2>;
+  }
+
+  return (
+    <>
+      <Jumbotron fluid className='text-light bg-dark'>
+        <Container>
+          <h1>Viewing saved books!</h1>
+        </Container>
+      </Jumbotron>
+      <Container>
+        <h2>
+          {userData.savedBooks.length
+            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+            : 'You have no saved books!'}
+        </h2>
+        <CardColumns>
+          {userData.savedBooks.map((book) => {
+            return (
+              <Card key={book.bookId} border='dark'>
+                {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+                  <p className='small'>Authors: {book.authors}</p>
+                  <Card.Text>{book.description}</Card.Text>
+                  <Button className='btn-block btn-danger' onClick={() => HandleDeleteBook(book.bookId)}>
+                    Delete this Book!
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </CardColumns>
+      </Container>
+    </>
+  );
+};
+
+export default SavedBooks;
