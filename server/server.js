8f81310114683b145228a0a6c3921317d21949b7
James Rostirolla <j_rostirolla@hotmail.com>
2021-10-26 22:55:59 +1100

    heroku update

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 //Added server
 const server = new ApolloServer({
   typeDefs,
